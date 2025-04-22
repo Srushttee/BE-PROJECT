b4a68986e29e9c38767acf26707d24dfe8cb18e3
WankhadeAnup <wankhadeanup94@gmail.com>
2025-04-22 23:21:16 +0530

    ml_model code v1.0

--- client/ml_model/rf_regressor_train.py
+++ client/ml_model/rf_regressor_train.py
@@ -1,3 +1,4 @@
+
 # train_model.py
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
@@ -6,7 +7,7 @@ from sklearn.model_selection import train_test_split
 import joblib
 
 # Load generated data
-df = pd.read_csv("D:\python based projects\ML data\electricity_tariff_fy25_clean.csv")
+df = pd.read_csv("electricity_tariff_clean.csv")
 
 # Sort for next month billing prediction
 df = df.sort_values(by=['customer_id', 'year', 'month'])
@@ -28,9 +29,9 @@ df['next_month_bill'] = df.groupby('customer_id')['billing_amount_inr'].shift(-1
 df = df.dropna()
 
 # Features and target
-features = ['region_encoded', 'year', 'month_num', 'household_size', 'num_appliances', 'units_consumed_kwh']
+features = ['region_encoded', 'year', 'month_num', ]
 X = df[features]
-y = df['next_month_bill']
+y = df['units_consumed_kwh']
 
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -40,5 +41,5 @@ model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 
 # Save the model
-#joblib.dump(model,'billing_predictor_model.pkl')
-print("✅ Model trained and saved as billing_predictor_model.pkl")
+joblib.dump(model,'billing_predictor_model.pkl')
+print(" Model trained and saved as billing_predictor_model.pkl")
